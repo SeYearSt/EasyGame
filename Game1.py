c2a22d1b37bebcaa1c34fe9b01cee39e39885734
justOleh <olehvenhryniuk@gmail.com>
2018-03-02 22:29:12 +0200

    added create blue circle by mouse click

--- Game1.py
+++ Game1.py
@@ -3,11 +3,26 @@ import Tkinter
 WIDTH = 300
 HEIGHT = 300
 BG_COLOR = "white"
+BL_COLOR = "blue"
+R = 30
+#class Ball
+class Balls():
+    def __init__(self, x, y, r, color, dx=0, dy=0):
+        self.x=x
+        self.y=y
+        self.r=r
+        self.color=color
+        self.dx=dx
+        self.dy=dy
+    def draw(self):
+        canvas.create_oval(self.x-self.r,self.y-self.r,self.x + self.r,self.y + self.r,fill = self.color)
+
 
 
 #mouse event
 def mouse_click(event):
-    print(event.num,event.x,event.y)
+    ball = Balls(event.x,event.y,R,BL_COLOR)
+    ball.draw()
 
 
 root = Tkinter.Tk()
